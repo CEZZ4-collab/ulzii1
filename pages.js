a16d81492368218e2452ef044cbe66374c9258fd
CEZZ4-collab <bulziiuchral74@gmail.com>
2025-12-22 10:32:13 -0800

    kk

--- pages.js
+++ pages.js
@@ -42,34 +42,3 @@
 
       
         gsap.from("nav", { y: -100, opacity: 0, duration: 1, delay: 0.2 });
-
-const modal = document.getElementById('loginModal');
-const trigger = document.getElementById('loginTrigger');
-const closeBtn = document.querySelector('.close-x');
-const box = document.querySelector('.login-box');
-
-trigger.addEventListener('click', () => {
-    modal.style.display = 'flex';
-    gsap.to(modal, { opacity: 1, duration: 0.4 });
-    gsap.fromTo(box, 
-        { scale: 0.8, y: 50, opacity: 0 }, 
-        { scale: 1, y: 0, opacity: 1, duration: 0.6, ease: "back.out(1.7)" }
-    );
-});
-
-
-const closeModal = () => {
-    gsap.to(modal, { 
-        opacity: 0, 
-        duration: 0.3, 
-        onComplete: () => { modal.style.display = 'none'; } 
-    });
-};
-
-closeBtn.addEventListener('click', closeModal);
-
-window.addEventListener('click', (e) => {
-    if (e.target === modal) {
-        closeModal();
-    }
-});
